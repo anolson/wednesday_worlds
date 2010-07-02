1258a45192b40b04faddb654cf457a4bb98db095
Andrew Olson <anolson@gmail.com>
2010-07-01 22:22:10 -0400

    Upadating development config with ActionMailer properties.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,14 @@ WednesdayWorlds::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = { 
+    :address => "smtp.gmail.com",  
+    :port => 587,  
+    :domain => 'wednesdayworlds.org',  
+    :user_name => ENV['ACTION_MAILER_USER_NAME'],  
+    :password => ENV['ACTION_MAILER_PASSWORD'],  
+    :authentication => 'plain',  
+    :enable_starttls_auto => true }
 end
