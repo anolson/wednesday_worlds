1b9e302510477910af342d45a2bca4819ac38613
Andrew Olson <anolson@gmail.com>
2010-09-07 22:13:07 -0400

    Fixing up tests.

--- test/functional/ride_mailer_test.rb
+++ test/functional/ride_mailer_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class RideMailerTest < ActionMailer::TestCase
   # replace this with your real tests
   test "week ride email" do
-    email = RideMailer.deliver
+    ride = Ride.find_by_currently_active(true)
+    email = RideMailer.weekly_ride_email(ride).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+    assert_equal ["vt_cycling@listserv.vt.edu"], email.to
   end
 end
--- test/functional/routes_controller_test.rb
+++ test/functional/routes_controller_test.rb
@@ -2,48 +2,19 @@ require 'test_helper'
 
 class RoutesControllerTest < ActionController::TestCase
   setup do
-    @route = routes(:one)
+    @ride = rides(:wednesday_worlds)
+    @route = routes(:tabor)
   end
 
   test "should get index" do
-    get :index
+    get :index, :controller => 'routes', :ride_id => @ride.id
     assert_response :success
     assert_not_nil assigns(:routes)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create route" do
-    assert_difference('Route.count') do
-      post :create, :route => @route.attributes
-    end
-
-    assert_redirected_to route_path(assigns(:route))
-  end
-
   test "should show route" do
-    get :show, :id => @route.to_param
+    get :show, :controller => 'routes', :ride_id => @ride.id, :id => @route.to_param
     assert_response :success
   end
-
-  test "should get edit" do
-    get :edit, :id => @route.to_param
-    assert_response :success
-  end
-
-  test "should update route" do
-    put :update, :id => @route.to_param, :route => @route.attributes
-    assert_redirected_to route_path(assigns(:route))
-  end
-
-  test "should destroy route" do
-    assert_difference('Route.count', -1) do
-      delete :destroy, :id => @route.to_param
-    end
-
-    assert_redirected_to routes_path
-  end
+  
 end
