d1c1a32f82679e306f3fd1c8739c3e9b5332b74b
Andrew Olson <anolson@gmail.com>
2011-01-04 15:52:29 -0500

    Adding Andrew Gold and The Owen Cup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,64 @@
 
 
 
-ride = Ride.create(
-  :name => 'Wednesday Worlds', 
+# ride = Ride.create(
+#   :name => 'Wednesday Worlds', 
+#   :location => 'Bollo\'s', 
+#   :currently_active => false)
+# 
+# tabor = Route.create(
+#   :name => 'Mt Tabor road', 
+#   :description => 'Out Mt Tabor Rd, back Blacksburg Rd', 
+#   :map_url => 'http://bit.ly/bSpQAT',
+#   :ride => ride)
+#   
+# tabor_occurrence = Occurrence.create(
+#   :recurs => true, :recurrence_type => 'bi-weekly', 
+#   :recurrence_ends_at => '2010-10-06 17:15', 
+#   :begins_at => '2010-05-05 17:15', 
+#   :route => tabor)
+# 
+# blacksburg_road = Route.create(
+#   :name => 'Blacksburg road', 
+#   :description => 'Out and back on Blacksburg rd.', 
+#   :map_url => 'http://bit.ly/bCG3Lf', 
+#   :ride => ride)
+#   
+# blacksburg_road_occurrence = Occurrence.create(
+#   :recurs => true, 
+#   :recurrence_type => 'bi-weekly', 
+#   :recurrence_ends_at => '2010-10-13 17:15', 
+#   :begins_at => '2010-05-12 17:15', :route => blacksburg_road);
+
+andrew_gold = Ride.create(
+  :name => 'Andrew Gold', 
   :location => 'Bollo\'s', 
   :currently_active => false)
 
-tabor = Route.create(
-  :name => 'Mt Tabor road', 
-  :description => 'Out Mt Tabor Rd, back Blacksburg Rd', 
+andrew_gold_route = Route.create(
+  :name => 'Andrew Gold route', 
+  :description => 'Out Blacksburg Rd, finish at the Blacksburg Town sign on Mt Tabor Rd', 
   :map_url => 'http://bit.ly/bSpQAT',
-  :ride => ride)
+  :ride => andrew_gold)
   
-tabor_occurrence = Occurrence.create(
-  :recurs => true, :recurrence_type => 'bi-weekly', 
-  :recurrence_ends_at => '2010-10-06 17:15', 
-  :begins_at => '2010-05-05 17:15', 
-  :route => tabor)
-
-blacksburg_road = Route.create(
-  :name => 'Blacksburg road', 
-  :description => 'Out and back on Blacksburg rd.', 
-  :map_url => 'http://bit.ly/bCG3Lf', 
-  :ride => ride)
+andrew_gold_occurrence = Occurrence.create(
+  :recurs => false, 
+  :begins_at => '2011-01-22 10:00', 
+  :route => andrew_gold_route)
   
-blacksburg_road_occurrence = Occurrence.create(
-  :recurs => true, 
-  :recurrence_type => 'bi-weekly', 
-  :recurrence_ends_at => '2010-10-13 17:15', 
-  :begins_at => '2010-05-12 17:15', :route => blacksburg_road);
-
+# owen_cup = Ride.create(
+#   :name => 'Andrew Gold', 
+#   :location => 'Bollo\'s', 
+#   :currently_active => false)
+# 
+# owen_cup_occurrence = Occurrence.create(
+#   :recurs => false, 
+#   :begins_at => '2011-01-29 10:00', 
+#   :route => owen_cup_route)
+#     
+# owen_cup_route = Route.create(
+#   :name => 'Andrew Gold route', 
+#   :description => 'Out Mt Tabor Rd, back Blacksburg Rd (3x).  Finish at the top of Harding', 
+#   :map_url => 'http://bit.ly/bSpQAT',
+#   :ride => owen_cup)
 
