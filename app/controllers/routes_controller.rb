29f8d626e73e33f8bea07a449865e013d55dcb7f
Andrew Olson <anolson@gmail.com>
2011-03-05 13:07:38 -0500

    Fixing routes index.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,8 +1,8 @@
 class RoutesController < ApplicationController
-  # GET /routes
-  # GET /routes.xml
+  # GET /rides/1/routes
+  # GET /rides/1/routes.xml
   def index
-    @routes = Route.all
+    @routes = Route.where(:ride_id => params[:ride_id])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -10,10 +10,12 @@ class RoutesController < ApplicationController
     end
   end
 
-  # GET /routes/1
-  # GET /routes/1.xml
+  # GET /rides/1/routes/1
+  # GET /rides/1/routes/1.xml
   def show
     @route = Route.find(params[:id])
+    
+    pp @route
 
     respond_to do |format|
       format.html # show.html.erb
