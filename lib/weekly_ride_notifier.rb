6bb5b30b0277b44a719c00a32a61433f2689e0fa
Andrew Olson <anolson@gmail.com>
2011-03-05 13:06:38 -0500

    Check that the ride occurs this week before sending the email/twitter blast.

--- lib/weekly_ride_notifier.rb
+++ lib/weekly_ride_notifier.rb
@@ -4,7 +4,7 @@ class WeeklyRideNotifier
   include 
   def self.send_weekly_ride_notification
     ride = Ride.find_by_currently_active(true)
-    if(ride)
+    if(ride && ride.this_weeks_route)
       #send the weekly ride tweet
       twitter_ride_notifier = TwitterRideNotifier.new
       twitter_ride_notifier.send_notification(ride)
