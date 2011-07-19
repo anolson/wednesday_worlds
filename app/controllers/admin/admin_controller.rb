595f8c75fac9035847a44857f3fb098f0fde1281
Andrew Olson <anolson@gmail.com>
2011-07-19 14:38:56 -0400

    Make methods private.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,16 +1,23 @@
 class Admin::AdminController < ApplicationController
     
   before_filter :verify_admin_authentication
+    
+  private
   
-  def verify_admin_authentication
-    unless signed_in?
-      session[:return_to] = request.fullpath
-      redirect_to login_path
+    def verify_admin_authentication
+      unless signed_in?
+        session[:return_to] = request.fullpath
+        redirect_to login_path
+      end
     end
-  end
+
+    def redirect_to_intended_path
+      session[:return_to] ? redirect_to(session[:return_to]) : redirect_to(admin_rides_path)
+      session[:return_to] = nil
+    end
   
-  def signed_in?
-    !session[:screen_name].nil?
-  end
-  helper_method "signed_in?"
+    def signed_in?
+      !session[:screen_name].nil?
+    end
+    helper_method "signed_in?"
 end
