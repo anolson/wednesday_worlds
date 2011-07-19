01933630d7a4429f8cfa25aa07816b710424f92a
Andrew Olson <anolson@gmail.com>
2011-07-19 11:09:14 -0400

    Handle twitter callback correctly.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,13 +1,16 @@
 class Admin::AdminController < ApplicationController
+    
+  before_filter :verify_admin_authentication
   
-  # def oauth_consumer
-  #   @oauth_consumer ||= OAuth::Consumer.new(ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'], :site => 'http://api.twitter.com', :request_endpoint => 'http://api.twitter.com', :sign_in => true)
-  # end
-  # 
+  def verify_admin_authentication
+    unless signed_in?
+      session[:return_to] = request.fullpath
+      redirect_to login_path
+    end
+  end
   
   def signed_in?
     !session[:screen_name].nil?
   end
   helper_method "signed_in?"
-  
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,13 +1,12 @@
 class Admin::SessionsController < Admin::AdminController
-  def new
-  end
   
-  def create
-    puts ENV['TWITTER_API_CONSUMER_KEY']
-    puts ENV['TWITTER_API_CONSUMER_SECRET']
-    
-    oauth_consumer = OAuth::Consumer.new(ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'], :site => 'http://api.twitter.com', :request_endpoint => 'http://api.twitter.com', :sign_in => true)
-    
+  skip_filter :verify_admin_authentication, :only => [:new, :create, :callback]
+  
+  rescue_from OAuth::Unauthorized, :with => :redirect_with_error
+  
+  def new; end
+  
+  def create   
     request_token = oauth_consumer.get_request_token(:oauth_callback => callback_admin_session_url)
     session['request_token'] = request_token.token
     session['request_secret'] = request_token.secret
@@ -17,17 +16,45 @@ class Admin::SessionsController < Admin::AdminController
   def callback
     request_token = OAuth::RequestToken.new(oauth_consumer, session['request_token'], session['request_secret'])
     access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-    reset_session
-    # session['access_token'] = access_token.token
-    # session['access_secret'] = access_token.secret
-    user = client.verify_credentials
-    session[:screen_name] = user.screen_name
-    # redirect_back_or root_path
-    
-    redirect_to new_admin_session
-    
+
+    user = client(access_token).verify_credentials
+    sign_in_user(user.screen_name)        
   end
   
   def destroy
   end
+  
+  private  
+    def sign_in_user(screen_name)
+      reset_session
+      
+      # Check that screen name is in the Admin table
+      # admin = Admininistrator.find_by_screen_name(screen_name)
+      
+      session[:screen_name] = screen_name
+      redirect_to login_path
+    end
+  
+    def redirect_with_error
+      reset_session
+      redirect_to login_path, :alert => "Awhoops.  Please signin again."
+    end
+    
+    def oauth_consumer
+      @oauth_consumer ||= OAuth::Consumer.new(ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'], :site => 'http://api.twitter.com', :request_endpoint => 'http://api.twitter.com', :sign_in => true)
+    end
+    
+    def client(access_token)
+      puts "Access token: #{access_token.token}"
+      puts "Access secret: #{access_token.secret}"
+      
+      Twitter.configure do |config|
+        config.consumer_key = ENV['TWITTER_API_CONSUMER_KEY']
+        config.consumer_secret = ENV['TWITTER_API_CONSUMER_SECRET']
+        config.oauth_token = access_token.token
+        config.oauth_token_secret = access_token.secret
+      end
+            
+      @client ||= Twitter::Client.new
+    end
 end
