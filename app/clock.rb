314b7d0d2d4c38ad4046662e7f488a61011d6fb1
Andrew Olson <anolson@gmail.com>
2011-10-19 13:34:34 -0400

    Re-org clockwork file, fix require for 1.9.x.

---
+++ app/clock.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__), '..', 'config', 'boot')
+require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
+
+every(1.day, 'notifications.send', :at => '10:15') { WeeklyRideNotifier.send_weekly_ride_notification if(Time.now.wday == 3) }
--- lib/clock.rb
+++
@@ -1,5 +0,0 @@
-require 'config/boot'
-require 'config/environment'
-require 'weekly_ride_notifier'
-
-every(1.day, 'notifications.send', :at => '10:15') { WeeklyRideNotifier.send_weekly_ride_notification if(Time.now.wday == 3) }
