16e89245cd5477fb242c95bf1865a33ad7060248
Andrew Olson <anolson@gmail.com>
2012-03-12 13:20:09 -0400

    Move notifiers back to app/lib

--- app/lib/weekly_ride_notifier.rb
+++ app/lib/weekly_ride_notifier.rb
@@ -1,11 +1,10 @@
-require 'twitter_ride_notifier'
-
 class WeeklyRideNotifier 
   def self.send_weekly_ride_notification
     ride = Ride.find_by_currently_active(true)
+    
     if(ride && ride.this_weeks_route)
       #send the weekly ride tweet
-      twitter_ride_notifier = TwitterRideNotifier.new
+      # twitter_ride_notifier = TwitterRideNotifier.new
       twitter_ride_notifier.send_notification(ride)
       
       #send the weekly ride e-mail
