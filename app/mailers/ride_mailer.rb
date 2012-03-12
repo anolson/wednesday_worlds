85385ed6ac559801e89dfc5fc66f548d6b2f0e28
Andrew Olson <anolson@gmail.com>
2012-03-12 11:27:35 -0400

    Good idea to close quotes.

--- app/mailers/ride_mailer.rb
+++ app/mailers/ride_mailer.rb
@@ -7,7 +7,7 @@ class RideMailer < ActionMailer::Base
   def weekly_ride_email(ride)
     @ride = ride
     
-    mail(:to => "anolson@gmail.com,
+    mail(:to => "anolson@gmail.com",
          :subject => "WednesdayWorlds - #{format_date(@ride.this_weeks_route.occurrence.this_weeks_date)}")
   end
 end
