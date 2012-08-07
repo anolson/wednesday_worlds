db2b463d3f927d77f3e058373cc74d02bc8c61b8
Andrew Olson <anolson@gmail.com>
2012-08-06 20:44:12 -0400

    Adding twitter notifications.

---
+++ app/lib/ride_tweet.rb
@@ -0,0 +1,31 @@
+class RideTweet
+  attr_accessor :ride
+
+  def initialize(ride)
+    @ride = ride
+  end
+
+  def to_s
+    %(#{date}. #{location}. #{time}. #{route} #fb.) 
+  end
+
+  def date
+    format_date(this_week.occurrence.next_date)
+  end
+
+  def location
+    ride.location
+  end
+
+  def time
+    format_time(this_week.occurrence.begins_at)
+  end
+
+  def route
+    this_week.name
+  end
+
+  def this_week
+    ride.routes.this_week
+  end
+end
--- app/models/twitter_notifier.rb
+++ app/models/twitter_notifier.rb
@@ -1,39 +1,11 @@
 class TwitterNotifier < Notifier
   def send_notification
-    client.update ride_tweet
+    client.update RideTweet.new(ride).to_s
   end
 
   private
 
-  #FIXME Move into initializer.
   def client
-    Twitter.configure do |config|
-      config.consumer_key = ENV['TWITTER_API_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_API_CONSUMER_SECRET']
-      config.oauth_token = ENV['TWITTER_API_ACCESS_TOKEN']
-      config.oauth_token_secret = ENV['TWITTER_API_ACCESS_SECRET']
-    end
     @client ||= Twitter::Client.new
-  end
-
-  # FIXME Refactor this.
-  def ride_tweet
-    %(#{date}. #{location}. #{time}. #{name} #fb.) 
-  end
-
-  def date
-    format_date(ride.routes.this_week.occurrence.next_date)
-  end
-
-  def location
-    ride.location
-  end
-
-  def time
-    format_time(ride.routes.this_week.occurrence.begins_at)
-  end
-
-  def name
-    ride.routes.this_week.name
   end
 end
