fa89848825aa17e86f071e4d30d9dedabc818752
Andrew Olson <anolson@gmail.com>
2013-01-18 20:38:13 -0500

    Bleh.

--- app/helpers/sections_helper.rb
+++
@@ -1,9 +0,0 @@
-module SectionsHelper
-  def section(title, header_link, partial)
-    render 'admin/common/section', section_options(title, header_link, partial)
-  end
-
-  def section_options(title, header_link, partial)
-    { title: title, header_link: header_link, partial: partial }
-  end
-end
