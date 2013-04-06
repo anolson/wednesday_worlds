83e4229e098e2b3fbb47f197cab42874935ef1d8
Andrew Olson <aolson@geezeo.com>
2013-04-05 22:14:51 -0400

    Manage notifiers.

--- app/controllers/admin/notifiers_controller.rb
+++ app/controllers/admin/notifiers_controller.rb
@@ -1,16 +1,40 @@
 class Admin::NotifiersController < Admin::AdminController
+  before_filter :find_ride
+
   def new
+    @notifier = Notifier.new
   end
 
   def create
+    if(@notifier = @ride.notifiers.create(params[:notifier]))
+      redirect_to admin_ride_url(@ride)
+    else
+      render :action => :new
+    end
   end
 
   def edit
+    @notifier = Notifier.find(params[:id])
   end
 
   def update
+    @notifier = Notifier.find(params[:id])
+
+    if(@notifier.update_attributes(params[:notifier]))
+      redirect_to admin_ride_url(@ride)
+    else
+      render :action => :edit
+    end
   end
 
   def destroy
+    Notifier.destroy(params[:id])
+    redirect_to admin_ride_url(@ride)
+  end
+
+  private
+
+  def find_ride
+    @ride = Ride.find(params[:ride_id])
   end
 end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,5 +1,11 @@
 class Notifier < ActiveRecord::Base
+  AVAILABLE_TYPES = [["Email", "EmailNotifier"], ["Twitter", "TwitterNotifier"]]
+
   belongs_to :ride
+
+  attr_accessible :recipient, :type, :enabled
+
+  validates_presence_of :recipient
 
   def send_notification
     raise NotImpementedError.new
