166201c6c892dce6ab37a4126cf5ffb569b8d56e
Andrew Olson <aolson@geezeo.com>
2013-04-17 22:36:00 -0400

    Fixing WeeklyRideNotifier.

--- app/lib/weekly_ride_notifier.rb
+++ app/lib/weekly_ride_notifier.rb
@@ -1,5 +1,7 @@
-class WeeklyRideNotifier 
+class WeeklyRideNotifier
   def self.send_weekly_ride_notification
-    ride.send_notifications if ride = Ride.find_by_currently_active(true)
+    if ride = Ride.find_by_currently_active(true)
+      ride.send_notifications
+    end
   end
 end
