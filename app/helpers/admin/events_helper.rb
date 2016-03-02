474b2f83a0494d247eaa140db00ada508febbd59
Andrew Olson <anolson@gmail.com>
2016-03-01 21:18:56 -0500

    Clean-up helpers.

--- app/helpers/admin/events_helper.rb
+++
@@ -1,5 +0,0 @@
-module Admin::EventsHelper
-  def link_to_new_event
-    link_to 'Create new event', new_admin_ride_event_path(@ride)
-  end
-end
--- app/helpers/admin/routes_helper.rb
+++
@@ -1,5 +0,0 @@
-module Admin::RoutesHelper
-  def link_to_new_route
-    link_to 'Create new route', new_admin_route_path
-  end
-end
