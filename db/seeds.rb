db004b22bd02f8170a4d48aeeed40a027cbf393a
Andrew Olson <anolson@gmail.com>
2016-04-29 21:22:02 -0400

    Update route map URLs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ ww = Ride.create(name: 'Wednesday Worlds', location: 'Bollo\'s')
 bburg_road = Route.create(
   name: 'Blacksburg road',
   description: bburg_copy,
-  map_url: 'http://bit.ly/bCG3Lf',
+  map_url: 'https://goo.gl/maps/ZaucV',
   slug: 'bburg')
 
 Event.create(:begins_at => "#{this_wednesday} 18:00", ride: ww, route: bburg_road)
@@ -18,18 +18,16 @@ Event.create(:begins_at => "#{this_wednesday} 18:00", ride: ww, route: bburg_roa
 tabor = Route.create(
   name: 'Mt Tabor road',
   description: tabor_copy,
-  map_url: 'http://bit.ly/bSpQAT',
+  map_url: 'https://goo.gl/maps/MS2kw',
   slug: 'tabor')
 
 Event.create(:begins_at => "#{next_wednesday} 18:00", ride: ww, route: tabor)
 
-tabor = Route.create(
+bburg_road_short = Route.create(
   name: 'Blacksburg Rd (short)',
   description: bburg_short_copy,
-  map_url: 'http://goo.gl/maps/UeFHu',
+  map_url: 'https://goo.gl/maps/uRRtKRmnsQw',
   slug: 'bburg-short')
 
 Administrator.create(:name => 'Andrew Olson', :twitter_screen_name => 'andrew_olson')
 EmailNotifier.create(:recipient => 'anolson@gmail.com', ride: ww)
-
-
