243f2985f0b7691c782e7d9d760f46c7ebe8dde3
Andrew Olson <anolson@gmail.com>
2017-02-12 21:40:14 -0500

    Configure RSpec.

---
+++ spec/rails_helper.rb
@@ -0,0 +1,16 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+end
