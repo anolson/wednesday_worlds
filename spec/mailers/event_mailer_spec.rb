bb466f7eb2da44dad06cc724276820bd2741beaf
Andrew Olson <anolson@gmail.com>
2017-03-02 22:10:39 -0500

    Fix spec.

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -3,13 +3,14 @@ require "rails_helper"
 RSpec.describe EventMailer do
   describe "notify" do
     let(:event) { create :event, :this_week }
-    let(:formatted_date) { event.begins_at.strftime("%b %e") }
+    let(:month) { event.begins_at.strftime("%b").strip }
+    let(:day) { event.begins_at.strftime("%e").strip }
     let(:recipient) { "wednesdayworlds@googlegroups.com" }
 
     subject(:mail) { EventMailer.event_notification(recipient, event) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("WednesdayWorlds - #{formatted_date}")
+      expect(mail.subject).to eq("WednesdayWorlds - #{month} #{day}")
       expect(mail.to).to eq([recipient])
       expect(mail.from).to eq(["hello@wednesdayworlds.org"])
     end
