746478aa4c78c43bf4b361ac9f106370e36c18d7
Andrew Olson <anolson@gmail.com>
2018-07-20 09:06:02 -0400

    WIP

---
+++ app/models/legacy.rb
@@ -0,0 +1,2 @@
+class Legacy
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,33 @@
 WednesdayWorlds::Application.routes.draw do
   namespace :admin do
-    namespace :legacy do
-      resource :dashboard, only: :show
+    # namespace :legacy do
+    #   resource :dashboard, only: :show
 
-      resource :session, :only => [:create, :new, :destroy] do
-         get :callback, :on => :member
-      end
+    #   resource :session, :only => [:create, :new, :destroy] do
+    #      get :callback, :on => :member
+    #   end
 
-      resources :rides do
-        resources :events
-        resources :notifiers
-      end
+    #   resources :rides do
+    #     resources :events
+    #     resources :notifiers
+    #   end
 
-      resources :routes
+    #   resources :routes
 
-      get '/' => redirect("/admin/legacy/dashboard")
-    end
+    #   get '/' => redirect("/admin/legacy/dashboard")
+    # end
+
+    resources :events
+    resources :notifiers
+    resources :rides
+    resources :routes
+    resources :users
+
+    root to: "events#index"
   end
 
-  get 'signin' => 'admin/legacy/sessions#new'
-  get 'signout' => 'admin/legacy/sessions#destroy'
+  # get 'signin' => 'admin/legacy/sessions#new'
+  # get 'signout' => 'admin/legacy/sessions#destroy'
 
   root "pages#index"
 end
