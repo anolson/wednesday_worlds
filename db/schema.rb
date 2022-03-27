ccbbb430ad6a35a81f635a549d7ae3dfe3e2934f
Andrew Olson <anolson@gmail.com>
2022-03-27 11:11:26 -0400

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2017_02_28_033544) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.datetime "begins_at"
     t.integer "ride_id"
