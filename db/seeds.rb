e0a3ca35782394528657e6ffda3f23e6945bb2f6
Andrew Olson <anolson@gmail.com>
2022-08-27 07:52:55 -0400

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ ww = Ride.create(name: 'Wednesday Worlds', location: 'Bollo\'s')
 bburg_road = Route.create(
   name: 'Blacksburg road',
   description: bburg_copy,
-  map_url: 'https://goo.gl/maps/ZaucV',
+  map_url: 'https://goo.gl/maps/YjHKcLoYh8Yzc88t5',
   slug: 'bburg')
 
 Event.create(begins_at: "#{this_wednesday} 18:00", ride: ww, route: bburg_road)
@@ -18,7 +18,7 @@ Event.create(begins_at: "#{this_wednesday} 18:00", ride: ww, route: bburg_road)
 tabor = Route.create(
   name: 'Mt Tabor road',
   description: tabor_copy,
-  map_url: 'https://goo.gl/maps/MS2kw',
+  map_url: 'https://goo.gl/maps/sPiLaSAyjfCdaUS8A',
   slug: 'tabor')
 
 Event.create(begins_at: "#{next_wednesday} 18:00", ride: ww, route: tabor)
@@ -26,7 +26,7 @@ Event.create(begins_at: "#{next_wednesday} 18:00", ride: ww, route: tabor)
 bburg_road_short = Route.create(
   name: 'Blacksburg Rd (short)',
   description: bburg_short_copy,
-  map_url: 'https://goo.gl/maps/uRRtKRmnsQw',
+  map_url: 'https://goo.gl/maps/41FaumNsBtPftyVm7',
   slug: 'bburg-short')
 
 User.create(email: 'anolson@gmail.com', password: 'secret')
