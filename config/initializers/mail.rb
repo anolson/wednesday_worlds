f1c4aa2da532b669e150f2fb6e11b1f20dd998e0
Andrew Olson <anolson@gmail.com>
2024-12-29 10:51:05 -0500

    Update action_mailer config.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
-WednesdayWorlds::Application.config.action_mailer.delivery_method = :smtp 
-WednesdayWorlds::Application.config.action_mailer.smtp_settings = { 
-  :address => "smtp.gmail.com",  
-  :port => 587,  
-  :domain => 'wednesdayworlds.org',  
-  :user_name => ENV['ACTION_MAILER_USER_NAME'],  
-  :password => ENV['ACTION_MAILER_PASSWORD'],  
-  :authentication => 'plain',  
-  :enable_starttls_auto => true }
+WednesdayWorlds::Application.config.action_mailer.delivery_method = :smtp
+WednesdayWorlds::Application.config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: Rails.application.credentials.action_mailer.domain,
+  user_name: Rails.application.credentials.action_mailer.user_name,
+  password: Rails.application.credentials.action_mailer.password,
+  authentication: "plain",
+  enable_starttls_auto: true }
