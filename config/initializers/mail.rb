32c8e22af9ff27751ddeaa3bca7da4af995e7fe8
Andrew Olson <anolson@gmail.com>
2024-12-29 11:23:36 -0500

    Move mailer config out of an initializer.

--- config/initializers/mail.rb
+++
@@ -1,9 +0,0 @@
-WednesdayWorlds::Application.config.action_mailer.delivery_method = :smtp
-WednesdayWorlds::Application.config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: Rails.application.credentials.action_mailer.domain,
-  user_name: Rails.application.credentials.action_mailer.user_name,
-  password: Rails.application.credentials.action_mailer.password,
-  authentication: "plain",
-  enable_starttls_auto: true }
